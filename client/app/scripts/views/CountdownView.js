92fdc7f5ee707c0f0912cefa81cc67ea6eadbb1f
Lonny Gomes <code@lonnygomes.com>
2014-05-06 20:17:19 -0400

    Incorporated the count down code that wasn't used last year
    - We need to find a better way of handling sub views with vanilla
      Backbone

---
+++ client/app/scripts/views/CountdownView.js
@@ -0,0 +1,41 @@
+/*jslint nomen: true */
+/*global define, _ */
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'models/CountdownModel',
+    'templates'
+], function ($, _, Backbone, CountdownModel, JST) {
+    'use strict';
+
+    var CountdownView = Backbone.View.extend({
+        template: JST['app/scripts/templates/CountdownView.hbs'],
+
+        tagName: 'div',
+
+        id: 'countdownContainer',
+
+        className: '',
+
+        events: {},
+
+        model: new CountdownModel(),
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+
+        },
+
+        render: function () {
+            //console.log("rendering count down view:" + this.$el);
+            //TODO: change to cached jquery selector
+            $("#countdownContainer").html(this.template(this.model.toJSON()));
+
+            return this;
+        }
+    });
+
+    return CountdownView;
+});
--- client/app/scripts/views/HomeView.js
+++ client/app/scripts/views/HomeView.js
@@ -5,27 +5,57 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'views/CountdownView',
     'templates'
-], function ($, _, Backbone, JST) {
+], function ($, _, Backbone, CountdownView, JST) {
     'use strict';
 
     var HomeViewView = Backbone.View.extend({
         template: JST['app/scripts/templates/HomeView.hbs'],
 
-        tagName: 'div',
-
         id: 'homeView',
+
+        $countdownEl: '',
 
         className: '',
 
         events: {},
 
+        countdownView: {},
+
         initialize: function () {
             //this.listenTo(this.model, 'change', this.render);
+            this.countdownView = new CountdownView();
+
+            //this.listenTo(this.countdownView.model, "change", this.renderCountdown);
         },
 
         render: function () {
+
+            //populate view and then insert contents of subview
             this.$el.html(this.template());
+
+            this.renderCountdown();
+
+            return this;
+        },
+
+        renderCountdown: function () {
+            //first render subview and get needed values
+            var countdownEl,
+                countdownSel;
+
+            this.countdownView.render();
+
+            if (!this.$countdownEl) {
+                //first render subview and get needed values
+                countdownEl = this.countdownView.$el;
+                countdownSel = "#" + countdownEl.attr("id");
+
+                this.$countdownEl = this.$el.find(countdownSel);
+                this.$countdownEl.html(this.countdownView.$el.html());
+            }
+
         }
     });
 
