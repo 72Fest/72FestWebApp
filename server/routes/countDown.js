ff7073c84d1321ca9f5817485d2d9b773bd570e7
Lonny Gomes <code@lonnygomes.com>
2014-05-07 20:53:40 -0400

    Added an endpoint to nodejs for the countdown REST call

---
+++ server/routes/countDown.js
@@ -0,0 +1,24 @@
+/*global require, module */
+var express = require('express');
+var router = express.Router();
+
+var countdownMetadata = {
+    caption: "Film Screening Countdown",
+    time: {
+        year: 2014,
+        month: 10,
+        day: 11,
+        hour: 18,
+        minute: 0,
+        second: 0
+    }
+};
+
+/* GET countdown. */
+router.get('/', function (req, res) {
+    "use strict";
+
+    res.jsonp(countdownMetadata);
+});
+
+module.exports = router;
