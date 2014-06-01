9ae029d045f99acbf9dbd00ba0d39a61ceee74e9
Lonny Gomes <code@lonnygomes.com>
2014-06-01 11:58:51 -0400

    Incorporated a basic node/mongoose photo api from another project
    - handles image upload, mongo persistence and listing images
    - grabbed the code from my node photo share server project
    - added formidiable and easyimage as npm depenencies

--- server/app.js
+++ server/app.js
@@ -12,9 +12,12 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/72Fest');
 var db = mongoose.connection;
 
+//routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var countDown = require('./routes/countDown');
+var ApiRouteManager = require('./routes/ApiRouteManager');
+var api = new ApiRouteManager(mongoose, app);
 
 var app = express();
 
@@ -34,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/countDown', countDown);
+app.use('/api', api.router);
 
 /// catch 404 and forwarding to error handler
 app.use(function (req, res, next) {
