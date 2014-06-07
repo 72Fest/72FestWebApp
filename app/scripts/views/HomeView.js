9401fb57836b31155aae75389198162342ad7d21
Lonny Gomes <code@lonnygomes.com>
2014-06-06 21:54:28 -0400

    Added prototype functionality of how the auth will work
    - it currently is not attempting to authenticate to the server
    - may change it from an authcode to an email address or something

--- app/scripts/views/HomeView.js
+++ app/scripts/views/HomeView.js
@@ -6,8 +6,10 @@ define([
     'underscore',
     'backbone',
     'views/CountdownView',
+    'collections/AuthTokenCollection',
+    'models/AuthTokenModel',
     'templates'
-], function ($, _, Backbone, CountdownView, JST) {
+], function ($, _, Backbone, CountdownView, AuthTokenCollection, AuthTokenModel, JST) {
     'use strict';
 
     var HomeViewView = Backbone.View.extend({
@@ -21,29 +23,77 @@ define([
 
         className: '',
 
-        events: {
-            "click #cameraBtn": function (e) {
-                alert("TODO: implement phonegap camera api");
-            },
-            "touchstart #cameraBtn": function () {
-                alert("???");
-            }
+        authCollection: null,
+
+        isAuth: function () {
+            this.authCollection.fetch();
+
+            //TODO: make this work with the server
+            return (this.authCollection.length > 0) ? true : false;
         },
+
+        events: {},
 
         countdownView: {},
 
         initialize: function () {
+            var that = this,
+                mangageAuth = function (e) {
+                    var authModel,
+                        userAuthInputVal = $("#authInputKey").val();
+
+                    if (!userAuthInputVal) {
+                        alert("Please input auth key!");
+                        return;
+                    }
+
+                    //stop propogation of click
+                    e.preventDefault();
+
+                    if (!that.isAuth()) {
+                        authModel = new AuthTokenModel({authToken: userAuthInputVal});
+
+                        that.authCollection.add(authModel);
+
+                        authModel.save();
+
+                        that.render();
+                    }
+
+                    //return false to make sure things
+                    return false;
+                };
             //this.listenTo(this.model, 'change', this.render);
             this.countdownView = new CountdownView();
 
             this.listenTo(this.countdownView.model, "change", this.renderCountdown);
+
+            //Home screen will stick around so we really
+            this.listenTo(this, "ready", function () {
+
+                $("#authForm").submit(mangageAuth);
+                $("#cameraBtn").click(function (e) {
+                    alert("TODO");
+                });
+            });
+
+            this.authCollection = new AuthTokenCollection();
+            this.authCollection.fetch();
+
+
         },
 
         render: function () {
+            var containerEl = $("#homeView");
+
+            //if we couldn't find it in the dom, set container to the $el
+            if (containerEl.length === 0) {
+                containerEl = this.$el;
+            }
             //remove cached version of jquery object
             this.$countdownContainerEl = null;
             //populate view and then insert contents of subview
-            this.$el.html(this.template({isAuth: false}));
+            containerEl.html(this.template({isAuth: this.isAuth()}));
             this.renderCountdown();
 
             return this;
