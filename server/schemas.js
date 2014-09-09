1e6a4eb78dcbf1ae4cb242db96b9e44a22578bcd
Lonny Gomes <code@lonnygomes.com>
2014-09-09 19:12:42 -0400

    Added Team schema

--- server/schemas.js
+++ server/schemas.js
@@ -1,11 +1,12 @@
 /*jslint nomen: true */
-/*global require, module */
+/*global require, exports, module */
 (function () {
     "use strict";
 
     var db,
         Photo,
         Vote,
+        Team,
         initPhotoSchema = function (mg) {
             var schema = mg.Schema({
                 size: Number,
@@ -28,19 +29,39 @@
 
             return schema;
         },
+        initTeamSchema = function (mg) {
+            var schema = mg.Schema({
+                teamName: String,
+                bio: String,
+                website: String,
+                logo: String,
+                films: [
+                    {
+                        title: String,
+                        year: Number,
+                        url: String
+                    }
+                ]
+            });
+
+            return schema;
+        },
         init = function (dbVal) {
             this.db = dbVal;
 
             //initialize schemas
             var photoSchema = initPhotoSchema(dbVal),
-                voteSchema = initVoteSchema(dbVal);
+                voteSchema = initVoteSchema(dbVal),
+                teamSchema = initTeamSchema(dbVal);
 
             //save schemas to namespace
             this.Photo = dbVal.model("Photo", photoSchema);
             this.Vote = dbVal.model("Vote", voteSchema);
+            this.Team = dbVal.model("Team", teamSchema);
         };
 
     exports.init = init;
     exports.Photo = Photo;
     exports.Vote = Vote;
+    exports.Team = Team;
 }());
