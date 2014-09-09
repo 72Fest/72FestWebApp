64f5b25e4024833229d9437a48dd78e712f4cfe0
Lonny Gomes <code@lonnygomes.com>
2014-09-09 19:36:42 -0400

    Added /logos mapping to server

--- server/app.js
+++ server/app.js
@@ -37,6 +37,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/photos", express.static(path.resolve(path.join(__dirname, 'public/photos'))));
+app.use("/logos", express.static(path.resolve(path.join(__dirname, 'public/logos'))));
 
 app.use('/', routes);
 app.use('/users', users);
