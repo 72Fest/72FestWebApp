11d81f8fc73fe719d159d26e3481a4d1dcd826f5
Lonny Gomes <code@lonnygomes.com>
2017-09-14 19:13:29 -0400

    Add registerEndPoint method for cloud module
    This method creates an endpoint in AWS and registers the endpoint with a specified topic.

--- server/lib/cloud.js
+++ server/lib/cloud.js
@@ -1,8 +1,12 @@
 
 const fs = require('fs');
 const AWS = require('aws-sdk');
+AWS.config.update({region:'us-east-1'});
+
 const s3 = new AWS.S3();
+const sns = new AWS.SNS();
 const path = require('path');
+const config = require('../config.json');
 
 module.exports = class CloudUtils {
     constructor(bucketName, basePath) {
@@ -36,6 +40,46 @@ module.exports = class CloudUtils {
                 }
 
                 resolve(data);
+            });
+        });
+    }
+
+    /**
+     * Registers mobile enpoint with AWS SNS service
+     * @param {string} tokenId - token or registration id supplied by notification service
+     * @param {string} platformType - mobile platform (iOS or Android)
+     */
+    registerEndPoint(tokenId, platformType) {
+        const params = {
+            PlatformApplicationArn: config.awsApplicationArn,
+            Token: tokenId,
+            CustomUserData: platformType
+        };
+
+        return new Promise((resolve, reject) => {
+            sns.createPlatformEndpoint(params, function(err, data) {
+                var subscribeParams = {
+                    Protocol: 'application',
+                    TopicArn: config.awsTopicArn
+                };
+
+                if (err) {
+                    reject(err.message);
+                } else {
+                    // assing mobile endpoint
+                    subscribeParams.Endpoint = data.EndpointArn;
+
+                    // subscribe endpoint to topic
+                    sns.subscribe(subscribeParams, function (err, subscribeData) {
+                        if (err) {
+                            console.log('Failed to subscribe');
+                            reject(err.message);
+                        } else {
+                            // returns subscribeData.SubscriptionArn
+                            resolve(subscribeData);
+                        }
+                    });
+                }
             });
         });
     }
