9b15f59143ce1de529fee84cef320eb7e9b03533
Lonny Gomes <code@lonnygomes.com>
2017-10-03 20:45:28 -0400

    Update backup script to copy to copy a stage dump
    This stage dump is used for new EC2 instances

--- scripts/backupMongo.sh
+++ scripts/backupMongo.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 BACKUP_FILE=`hostname`-72fest-backup.zip
+S3_PATH="s3://72fest-backups/prod/mongo/2017"
+
+# dump contents of db
 mongodump --db 72Fest --out backup
 zip -r ${BACKUP_FILE} backup/72Fest/
 rm -rf backup
-aws s3 cp ${BACKUP_FILE} s3://72fest-backups/prod/mongo/2017/
+# save in s3
+aws s3 cp ${BACKUP_FILE} ${S3_PATH}/
+# copy file to latests which other EC2 instances will use
+aws s3 cp ${S3_PATH}/${BACKUP_FILE} ${S3_PATH}/latest-72fest-backup.zip
 rm ${BACKUP_FILE}
